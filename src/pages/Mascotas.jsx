07599d4104a013d81d707fa7e28c70a5bd0b152a
Guillermo Guzman Vargas <nemerfrozen@gmail.com>
2022-11-15 19:10:41 -0500

    Update Mascotas.jsx

--- src/pages/Mascotas.jsx
+++ src/pages/Mascotas.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import MascotaCard from "../components/MascotaCard"
 import Buscador from "../components/Buscador"
-import { API } from "../services/API"
+import { get } from "../services/API"
 import './Mascotas.css'
 
 const Mascotas = () => {
@@ -16,7 +16,7 @@ const Mascotas = () => {
     )
 
     const getAllMascotas = async () => {
-        API.get("/pets").then((resMascotas) => {
+        get("/pets").then((resMascotas) => {
             setMascotas(resMascotas.data.results.mascotas)
         })
     }
